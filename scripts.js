2e63afb0a28d99840a4e01438ebd5c5878198810
Juan Souza <ssouza.juann@gmail.com>
2025-05-12 17:52:24 -0300

    Primeiro commit do projeto Starbucks

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let circulo = document.querySelector(".circulo")
+let imagem = document.querySelector(".copao")
+
+function trocarcor(cor){
+    circulo.style.background = cor    
+}
+
+function trocaImagem(img){
+    imagem.src = img
+}
